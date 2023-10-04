06a06b085cb5142dd74cd5c152f9b427631ba98f
Миша Тарачков <mishklgpmi@mail.ru>
2023-10-04 16:59:01 +0200

    Add: материалы лекций 5 для обоих потоков

---
+++ advanced/lecture5/main.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int my_strlen(char* str);
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	// Конфигурационные параметры
+	const int N = 100;
+	const int M = 100;
+
+	char ch = 'a'; // Символ
+	char str[10] = "Hello1234";
+	//str[9] = 'a';
+	std::cout << str << std::endl;
+
+	// длина строки
+	std::cout << strlen(str) << std::endl;
+
+	// копирование строк
+	char str_copy[10];
+	strcpy_s(str_copy, str);
+
+	// конкатенация == сложение
+	char str_concat[20];
+	strcpy_s(str_concat, str);
+	strcat_s(str_concat, str_copy);
+
+	std::cout << str_concat << std::endl;
+
+	// сравнение
+	std::cout << strcmp("BBC", "bbc") << std::endl;
+
+	char cmp1[4] = "BBC";
+	char cmp2[4] = "bbc";
+	for(int i=0;i<4;i++)
+		if (66 < 98)
+		{
+
+		}
+
+	// лексикографический порядок
+
+	// ввод строк
+	std::ifstream in("input.txt"); // идиома RAII
+
+	if (!in.is_open())
+	{
+		std::cerr << "Open file error" << std::endl;
+		return -1;
+	}
+
+	// Посимвольный ввод
+	while (!in.eof())
+	{
+		char c;
+		in >> c;
+		std::cout << c << std::endl;
+	}
+
+	in.clear();
+	in.seekg(0);
+
+	// Ввод по словам
+	while (!in.eof())
+	{
+		char s[100];
+		in >> s;
+		std::cout << s << std::endl;
+	}
+
+	in.clear();
+	in.seekg(0);
+
+	// Ввод по строкам
+	while (!in.eof())
+	{
+		char line[10000];
+		in.getline(line, 10000);
+		std::cout << "<" << line << ">" << std::endl;
+	}
+
+	// ввод русского текста
+
+	// простейшие функции
+	std::cout << my_strlen(str_concat) << std::endl;
+
+	return 0;
+}
+
+int my_strlen(char* str)
+{
+	int length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
---
+++ basic/lecture5/main.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+//#define N_Max 3000
+
+int main()
+{
+	// Нам дана последовательность до N_max элементов
+	// Отсортировать по последней цифре числа, если цифры равны, то отсортировать
+	// по самому числу
+
+	/*const int N_Max = 3000;
+	int n;
+	std::cin >> n;
+
+	// массив фиксированной длины
+	int mas[N_Max];
+	int last_digit[N_Max];
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> mas[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		last_digit[i] = mas[i] % 10;
+	}
+
+
+	// Сортировка swap-sort
+	for(int i=0;i<n-1;i++)  // n
+		for(int j=i+1;j<n;j++) // n/2
+			if ((last_digit[i] > last_digit[j]) ||
+				(last_digit[i] == last_digit[j] && mas[i] > mas[j]))
+			{
+				int tmp = mas[i];
+				mas[i] = mas[j];
+				mas[j] = tmp;
+				tmp = last_digit[i];
+				last_digit[i] = last_digit[j];
+				last_digit[j] = tmp;
+			}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << mas[i] << " ";
+	}*/
+
+	const int N_Max = 3;
+	const int M_Max = 3;
+
+	int matrixA[N_Max][M_Max], matrixB[N_Max][M_Max], result[N_Max][M_Max];
+	int n, m;
+	std::cin >> n >> m;
+
+	if (n > N_Max || m > M_Max)
+	{
+		std::cout << "Dimension error!" << std::endl;
+		return -1;
+	}
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			std::cin >> matrixA[i][j];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			std::cin >> matrixB[i][j];
+
+	// Перемножение
+	//int p = matrix[0][0] * matrix[0][2];
+
+	// Транспонирование
+	/*for (int i = 0; i < n-1; i++)
+		for (int j = i + 1; j < m; j++)
+		{
+			int tmp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = tmp;
+		}*/
+
+	// Умножение
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+		{
+			result[i][j] = 0;
+			for (int k = 0; k < n; k++)
+			{
+				//std::cout << "i = " << i << " j = " << j << std::endl;
+				result[i][j] += matrixA[i][k] * matrixB[k][j];
+				//std::cout << result[i][j] << " " << matrixA[i][k] << " " << matrixB[k][j] << std::endl;
+			}
+		}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			std::cout << result[i][j] << " ";
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
