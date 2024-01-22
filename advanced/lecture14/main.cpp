89eb22dd4a42731e74f3aa353989f723f622a668
Миша Тарачков <mishklgpmi@mail.ru>
2024-01-22 18:33:27 +0200

    Add: лекция 14 + материалы

---
+++ advanced/lecture14/main.cpp
@@ -0,0 +1,137 @@
+﻿#include <iostream>
+#include <mutex>
+
+class String
+{
+	char* m_str;
+	size_t m_size; // size_t == unsigned long long
+
+public:
+	String(const char* s) 
+	{
+		std::cout << "Constr" << std::endl;
+
+		m_size = strlen(s) + 1;
+		m_str = new char[m_size];
+		
+		// Неэффективно
+		//for (size_t i = 0; i < m_size; i++)
+		//	m_str[i] = s[i];
+		// std::copy, memcpy
+		// Копирует по 8 байтов
+		std::copy(s, s + m_size, m_str);
+	}
+	
+	// Правило трех (Rules of Three): деструктор, констр. копирования, оператор присваивания копир.
+	// Конструктор копирования
+	String(const String& other) : String(other.m_str)
+	{
+		std::cout << "Copy constr" << std::endl;
+
+		/*
+		// Без делегирующего конструктора
+		m_size = other.m_size;
+		m_str = new char[m_size];
+		std::copy(other.m_str, other.m_str + m_size, m_str);*/
+	}
+
+	// Оператор присваивания копированием
+	// слева - this, справа - other
+	// s2 = (s2 = s1);
+
+private:
+	void Swap(String& s)
+	{
+		std::swap(m_str, s.m_str);
+		std::swap(m_size, s.m_size);
+	}
+
+public:
+	String& operator=(String copy)
+	{
+		std::cout << "Copy operator " << m_str << std::endl;
+		Swap(copy);
+		return *this;
+	}
+
+	// move-семантика
+	// Конструктор перемещения
+	// Оператор присваивания перемещением
+
+	void Print() const
+	{
+		// константный указатель
+		//char const* p1;
+		//m_str = nullptr; ERROR
+		// m_str[1] = 2; OK
+		// указатель на константу
+		//const char* p2;
+		// m_str = nullptr; OK
+		// m_str[1] = 2; ERROR
+		// константный указатель на константу
+		//const char* const p3 = m_str;
+
+		// String const& s
+		// const String&
+		std::cout << m_str << std::endl;
+	}
+
+	~String() 
+	{
+		std::cout << "Destr" << std::endl;
+		delete[] m_str;
+	}
+};
+
+void f(String s)
+{
+	s.Print();
+}
+
+struct Longitude
+{
+private:
+	double m_value;
+public:
+	explicit Longitude(double value)
+	{
+		m_value = value;
+	}
+};
+
+struct Latitude
+{
+private:
+	double m_value;
+public:
+	explicit Latitude(double value)
+	{
+		m_value = value;
+	}
+};
+
+void g(Latitude lat, Longitude longitude)
+{
+	// ...
+}
+
+int main() {
+	//Latitude lat(54.123);
+	//Longitude longitude(20.5);
+	//g(lat, longitude);
+
+	String s1("Hello");
+	//s1.Print();
+	//f("Hello");
+
+	String s3("abc");
+
+
+	String s2 = "111";		// Конструктор копирования
+	s3 = s2 = s1;			// Оператор присваивания копированием
+	s2.Print();
+
+	//s3 = s2 + 'a';
+
+	return 0;
+}
---
+++ basic/lecture14_intro_oop/main.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+
+// Хранение данных
+struct Point {
+	// Поля
+	int x;
+	int y;
+};
+
+// 
+class Circle {
+private:
+	// Поля
+	float m_r;
+	Point m_p;
+public:
+	// Методы
+	// Конструктор
+	Circle(float r, Point p)
+	{
+		std::cout << "Constr" << std::endl;
+		m_r = r; // (*this).r
+		m_p = p;
+	}
+
+	~Circle()
+	{
+		std::cout << "Destr" << std::endl;
+	}
+
+	// Сеттеры
+	void setR(float r)
+	{
+		if (r < 0)
+		{
+			std::cout << "Negative radius" << std::endl;
+			return;
+		}
+		m_r = r;
+	}
+
+	// Геттеры
+	float getR()
+	{
+		return m_r;
+	}
+
+	float Square()
+	{
+		return acos(-1) * m_r * m_r;
+	}
+};
+
+void Dist(Point p1, Point p2)
+{
+	// ...
+}
+
+void f(Circle c)
+{
+	std::cout << c.Square() << std::endl;
+}
+
+class RectPlus {
+	int m_a, m_b, m_c;
+	std::string m_color;
+
+public:
+	// Конструктор по умолчанию
+	//RectPlus() {}
+	RectPlus() = default;
+	RectPlus(int a, int b, int c) {
+		setA(a);
+		setB(b);
+		setC(c);
+	}
+
+	void setA(int a)
+	{
+		if (a < 0)
+			std::cout << "Negative A" << std::endl;
+		m_a = a;
+	}
+
+	void setB(int b)
+	{
+		if (b < 0)
+			std::cout << "Negative B" << std::endl;
+		m_b = b;
+	}
+
+	void setC(int c)
+	{
+		if (c < 0)
+			std::cout << "Negative C" << std::endl;
+		m_c = c;
+	}
+
+	int Volume()
+	{
+		return m_a * m_b * m_c;
+	}
+};
+
+class Matrix {
+	int m_n, m_m;
+	int** m_mat;
+
+public:
+	Matrix(int n, int m)
+	{
+		setN(n);
+		setM(m);
+		m_mat = new int* [n];
+		for (int i = 0; i < n; i++)
+			m_mat[i] = new int[m];
+	}
+
+	void setN(int n)
+	{
+		if (n <= 0)
+		{
+			std::cout << "Negative" << std::endl;
+			exit(-1);
+		}
+		m_n = n;
+	}
+
+	void setM(int m)
+	{
+		if (m <= 0)
+		{
+			std::cout << "Negative" << std::endl;
+			exit(-1);
+		}
+		m_m = m;
+	}
+
+	void Read()
+	{
+		for (int i = 0; i < m_n; i++)
+			for (int j = 0; j < m_m; j++)
+				std::cin >> m_mat[i][j];
+	}
+
+	~Matrix()
+	{
+		for (int i = 0; i < m_n; i++)
+			delete[] m_mat[i];
+		delete m_mat;
+	}
+};
+
+int main()
+{
+	//Matrix mat(2, 2);
+	//mat.Read();
+
+	//std::ifstream in("input.txt");
+
+	//FILE* f = fopen("input.txt", "r");
+
+	//fclose(f);
+
+
+	/*RectPlus r;
+	r.setA(1);
+	r.setB(2);
+	r.setC(3);
+	std::cout << r.Volume() << std::endl;*/
+
+
+	/*Point p1 = {0, 0};
+
+	// Circle - класс, с1 - объект, экземпляр класса
+	Circle c1(1, p1);
+	c1.setR(-10);
+
+	std::cout << c1.getR() << std::endl;
+	std::cout << c1.Square() << std::endl;*/
+
+	return 0;
+}
